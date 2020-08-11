c8646a677343912aaecdc5d1269744db29654f44
Duy <duyvukhanh@gmail.com>
2020-08-11 18:39:43 +0700

    react create app

---
+++ session_5/todo-list/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Di Cho</li>
+        <li>Nau Com</li>
+        <li>Rua Bat</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
